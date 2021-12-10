52a13d9491a3f624f7b747e07b66eb5e640e17a1
Daniel Geiger <daniel.w.geiger@gmail.com>
2021-12-10 15:53:32 -0700

    1-B solution; 2-A file setup

--- 1-A/1-A.js
+++ 1-A/1-A.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const depthData = fs.readFileSync("./depth-data.txt", 'utf-8');
+const depthData = fs.readFileSync("./1-A/depth-data.txt", 'utf-8');
 const stringArray = depthData.split('\n')
 
 const depthArray = [];
